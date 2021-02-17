680ec605217151590c575960ab458a9c5c6020ef
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-02-17 21:33:12 +0530

    Add refreshTokens

--- server/utils/Auth.js
+++ server/utils/Auth.js
@@ -6,14 +6,11 @@ const User = require("../models/User");
 const authenticate = (next) => async (parent, args, context, info) => {
   const { cookies } = context;
   const accessToken = cookies["access-token"];
-  const refreshToken = cookies["refresh-token"];
-  console.log({ accessToken, refreshToken });
-  if (!accessToken || !refreshToken) {
+  if (!accessToken) {
     throw new AuthenticationError("Unauthenticated");
   }
   try {
     const { id: accessTokenId } = verify(accessToken, process.env.JWT_SECRET);
-    const { id: refreshTokenId } = verify(refreshToken, process.env.JWT_SECRET);
     const user = await User.find({ _id: accessTokenId });
     if (!user) {
       throw new AuthenticationError("Unauthenticated");
@@ -22,9 +19,28 @@ const authenticate = (next) => async (parent, args, context, info) => {
     context.user = user[0];
     return next(parent, args, context, info);
   } catch (err) {
-    let errMessage = "Authetication failed";
+    let errMessage = "Authetication Failed";
     if (err.message === "jwt expired") {
-      errMessage = "JWT Expired";
+      try {
+        const refreshToken = cookies["refresh-token"];
+        if (!refreshToken) {
+          throw new AuthenticationError("Unauthenticated");
+        }
+        const { id: refreshTokenId } = verify(
+          refreshToken,
+          process.env.JWT_SECRET
+        );
+        const user = await User.find({ _id: refreshTokenId });
+        if (!user) {
+          throw new AuthenticationError("Unauthenticated");
+        }
+        //TODO: Re-new access token
+        if (!next) return user[0];
+        context.user = user[0];
+        return next(parent, args, context, info);
+      } catch (error) {
+        throw new AuthenticationError("Authentication Failed");
+      }
     }
     throw new AuthenticationError(errMessage);
   }
