9b38430ade00513830ec3dffcece417699a3cf62
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-02-18 16:04:31 +0530

    Renew token function done

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ Features:
 This branch is for **cookie** based authentication.
 
 Using Refresh token + Access Token
+
+The code does not store any refresh token data on the Database.
+
+The only way the token is invalidated is when it expires. The main motivation to do this is so one can see how to handle cookies using Apollo + NextJS, not how to implement more secure Authentication.
--- server/utils/Auth.js
+++ server/utils/Auth.js
@@ -1,14 +1,12 @@
 const { verify } = require("jsonwebtoken");
 const { AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
 const authenticate = (next) => async (parent, args, context, info) => {
   const { cookies } = context;
   const accessToken = cookies["access-token"];
-  if (!accessToken) {
-    throw new AuthenticationError("Unauthenticated");
-  }
   try {
     const { id: accessTokenId } = verify(accessToken, process.env.JWT_SECRET);
     const user = await User.find({ _id: accessTokenId });
@@ -19,30 +17,35 @@ const authenticate = (next) => async (parent, args, context, info) => {
     context.user = user[0];
     return next(parent, args, context, info);
   } catch (err) {
-    let errMessage = "Authetication Failed";
-    if (err.message === "jwt expired") {
-      try {
-        const refreshToken = cookies["refresh-token"];
-        if (!refreshToken) {
-          throw new AuthenticationError("Unauthenticated");
-        }
-        const { id: refreshTokenId } = verify(
-          refreshToken,
-          process.env.JWT_SECRET
-        );
-        const user = await User.find({ _id: refreshTokenId });
-        if (!user) {
-          throw new AuthenticationError("Unauthenticated");
-        }
-        //TODO: Re-new access token
-        if (!next) return user[0];
-        context.user = user[0];
-        return next(parent, args, context, info);
-      } catch (error) {
-        throw new AuthenticationError("Authentication Failed");
+    try {
+      const refreshToken = cookies["refresh-token"];
+      if (!refreshToken) {
+        throw new AuthenticationError("Unauthenticated");
       }
+      const { id: refreshTokenId } = verify(
+        refreshToken,
+        process.env.JWT_SECRET
+      );
+      console.log("rt function ran");
+      const user = await User.find({ _id: refreshTokenId });
+      if (!user) {
+        throw new AuthenticationError("Unauthenticated");
+      }
+      //Re-new access token
+      const token = jwt.sign({ id: user[0]._id }, process.env.JWT_SECRET, {
+        expiresIn: "15min",
+      });
+      context.res.cookie("access-token", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 15, // 15 Minutes
+        secure: process.env.NODE_ENV === "production",
+      });
+      if (!next) return user[0];
+      context.user = user[0];
+      return next(parent, args, context, info);
+    } catch (error) {
+      throw new AuthenticationError("Authentication Failed");
     }
-    throw new AuthenticationError(errMessage);
   }
 };
 
